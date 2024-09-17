ecc0c4768b6ae8617c40cc1c8bfc63a7a601178f
abdoasofi <asofi@bonya-ye.org>
2024-09-17 10:30:27 +0300

    Add def create_customer

--- taakd_app/hooks.py
+++ taakd_app/hooks.py
@@ -114,9 +114,10 @@ app_license = "mit"
 # ---------------
 # Override standard doctype classes
 
-# override_doctype_class = {
-# 	"ToDo": "custom_app.overrides.CustomToDo"
-# }
+override_doctype_class = {
+	  "Customer":"taakd_app.taakd_app.overrides.custom_customer.CustomCustomer",
+
+}
 
 # Document Events
 # ---------------
