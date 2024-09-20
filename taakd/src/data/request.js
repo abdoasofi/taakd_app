b36f6d4ad1b9fc210e94dfd8f0d7fa1e00be425c
abdoasofi <asofi@bonya-ye.org>
2024-09-20 23:28:16 +0300

    Add File request

---
+++ taakd/src/data/request.js
@@ -0,0 +1,14 @@
+// import router from '@/router'
+// import { createResource } from 'frappe-ui'
+
+// export const userResource = createResource({
+//   url: 'frappe.auth.get_logged_usr',
+//   cache: 'User',
+//   onError(error) {
+//     if (error && error.exc_type === 'AuthenticationError') {
+//       router.push({ name: 'LoginPage' })
+//     }
+//   },
+// })
+
+
