2c08d43838bd372b76331854cb9e1d9b9ac6a589
abdoasofi <asofi@bonya-ye.org>
2024-09-29 17:47:23 +0300

    Add Components ErrorMessage

---
+++ taakd/src/components/ErrorMessage.vue
@@ -0,0 +1,19 @@
+<!-- src/components/ErrorMessage.vue -->
+<template>
+    <div class="text-red-500">
+      {{ message }}
+    </div>
+  </template>
+  
+  <script setup>
+  const props = defineProps({
+    message: {
+      type: String,
+      required: true
+    }
+  })
+  </script>
+  
+  <style scoped>
+  /* يمكنك إضافة أنماط إضافية إذا لزم الأمر */
+  </style>
