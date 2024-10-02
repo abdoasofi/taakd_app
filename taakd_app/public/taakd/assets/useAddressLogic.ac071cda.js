ee48521d41ea381ae2e3fd976f045e01951b0f83
abdoasofi <asofi@bonya-ye.org>
2024-10-02 17:48:08 +0300

    fix frontevd

--- taakd_app/public/taakd/assets/useAddressLogic.ac071cda.js
+++
@@ -1,1 +0,0 @@
-import{r as o,d as t}from"./vendor.1aaec15d.js";o("");o("");o("");o("");o("");const e=t({type:"list",doctype:"Location",fields:["location_name","parent_location","location_type"],cache:"location",auto:!0});export{e as l};
