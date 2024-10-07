b019be7692ce34744b7af0a55fa66d9d7073a680
abdoasofi <asofi@bonya-ye.org>
2024-10-07 23:00:09 +0300

    Edit stepico

--- taakd/src/pages/steps.vue
+++ taakd/src/pages/steps.vue
@@ -37,11 +37,45 @@
   </div>
   <div class="bg-[#F0F0F0] fixed lg:static bottom-0 w-screen h-fit">
     <div class="container py-4 flex w-full justify-between items-center">
-      <div class="text-xs text-[#1D1B20]">Auto Saving</div>
+      <div class="lg:flex justify-start grow lg:gap-2" >
+             <svg width="30" height="30" viewBox="0 0 81 81" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect x="14.0355" y="40.5156" width="37.449" height="37.449" transform="rotate(-45 14.0355 40.5156)" stroke="#F7F7F7" stroke-width="5"/>
+        <mask id="mask0_175_7016" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="7" y="7" width="67" height="67">
+        <rect width="66.1225" height="66.1225" transform="translate(7.64355 7.23535)" fill="#DE3500"/>
+        </mask>
+        <g mask="url(#mask0_175_7016)">
+        <rect x="14.0365" y="40.5166" width="37.449" height="37.449" transform="rotate(-45 14.0365 40.5166)" stroke="#81C045" stroke-width="5"/>
+        </g>
+      </svg>
+      <div class="text-xs text-[#1D1B20]">Auto Saving</div> 
+      </div>
 
+      
       <div class="flex gap-2">
-        <button class="text-secondary" @click="previousStep" :disabled="currentStepIndex === 0">
-          &larr;
+        <button class="text-secondary" @click="previousStep" :disabled="currentStepIndex === 0" v-if="currentStepIndex != 0">
+          <svg width="86" height="62" viewBox="0 0 86 62" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_d_478_20124)">
+            <g clip-path="url(#clip0_478_20124)">
+            <path d="M38.373 30.75H49.75C49.9628 30.75 50.141 30.6782 50.2845 30.5345C50.4282 30.391 50.5 30.2128 50.5 30C50.5 29.7872 50.4282 29.609 50.2845 29.4655C50.141 29.3218 49.9628 29.25 49.75 29.25H38.373L43.5423 24.0808C43.6909 23.9321 43.7643 23.7581 43.7625 23.5588C43.7605 23.3594 43.682 23.1822 43.527 23.027C43.3718 22.8822 43.1962 22.8072 43 22.802C42.8038 22.7968 42.6282 22.8718 42.473 23.027L36.1327 29.3673C36.0391 29.4609 35.9731 29.5597 35.9348 29.6635C35.8962 29.7673 35.877 29.8795 35.877 30C35.877 30.1205 35.8962 30.2327 35.9348 30.3365C35.9731 30.4403 36.0391 30.5391 36.1327 30.6328L42.473 36.973C42.6115 37.1115 42.783 37.1823 42.9875 37.1855C43.192 37.1887 43.3718 37.1178 43.527 36.973C43.682 36.8178 43.7595 36.6397 43.7595 36.4385C43.7595 36.2372 43.682 36.0589 43.527 35.9038L38.373 30.75Z" fill="#81C045"/>
+            </g>
+            <path d="M7 30C7 16.7452 17.7452 6 31 6H55C68.2548 6 79 16.7452 79 30C79 43.2548 68.2548 54 55 54H31C17.7452 54 7 43.2548 7 30Z" stroke="#81C045" stroke-width="2" shape-rendering="crispEdges"/>
+            </g>
+            <defs>
+            <filter id="filter0_d_478_20124" x="0" y="0" width="86" height="62" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+            <feOffset dy="1"/>
+            <feGaussianBlur stdDeviation="3"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.32 0"/>
+            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_478_20124"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_478_20124" result="shape"/>
+            </filter>
+            <clipPath id="clip0_478_20124">
+            <path d="M7 30C7 16.7452 17.7452 6 31 6H55C68.2548 6 79 16.7452 79 30C79 43.2548 68.2548 54 55 54H31C17.7452 54 7 43.2548 7 30Z" fill="white"/>
+            </clipPath>
+            </defs>
+          </svg>
         </button>
         <Button 
           v-if="!isLastStep" 
@@ -52,7 +86,7 @@
         </Button>
       </div>
 
-      <div class="lg:flex justify-end grow lg:gap-2">
+      <div class="lg:flex justify-end grow lg:gap-2" v-if="isLastStep">
         <Button level="other" @clicked="accept" >
           Accept
         </Button>
--- taakd/src/pages/stepsSections/components/stepIcon.vue
+++ taakd/src/pages/stepsSections/components/stepIcon.vue
@@ -1,8 +1,13 @@
+<!-- stepico.vue -->
 <template>
         <div :class="['p-2 flex flex-col lg:flex-row lg:gap-2 lg:justify-start justify-center items-center',processClass]">
             <div :class="classes">
                 <span v-if="complete===false">0/32</span>
-                <span class=" border-[1px] border-white rounded-full w-[90%] h-[90%] flex justify-center items-center" v-if="complete===true"><span>T</span></span>
+                <span class=" border-[1px] border-white rounded-full w-[90%] h-[90%] flex justify-center items-center" v-if="complete===true"><span>
+					<svg width="18" height="13" viewBox="0 0 18 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M2.45101 5.69168L6.51626 9.75737L15.5152 0.757996C15.7689 0.504537 16.0764 0.377808 16.4374 0.377808C16.7985 0.377808 17.1059 0.504537 17.3597 0.757996C17.6132 1.01175 17.7371 1.32193 17.7316 1.68856C17.7257 2.05547 17.6018 2.36012 17.3597 2.60249L7.62357 12.3386C7.30711 12.6548 6.93801 12.8129 6.51626 12.8129C6.09451 12.8129 5.7254 12.6548 5.40895 12.3386L0.606506 7.53618C0.353048 7.28243 0.226318 6.97502 0.226318 6.61393C0.226318 6.25285 0.353048 5.94543 0.606506 5.69168C0.860257 5.43823 1.17044 5.31427 1.53707 5.31981C1.90399 5.32564 2.20863 5.4496 2.45101 5.69168Z" fill="white"/>
+					</svg>
+				</span></span>
             </div>
             <div class="lg:flex lg:flex-col justify-center mt-2 lg:mt-0">
                 <span class="block text-center lg:text-left">{{ label }}</span>
