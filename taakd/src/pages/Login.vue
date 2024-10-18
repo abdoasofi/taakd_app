4ed46ac6739ad246435a09c78c7e6bb559cb41f4
abdoasofi <asofi@bonya-ye.org>
2024-10-18 10:24:03 +0300

    Edit Logen

--- taakd/src/pages/Login.vue
+++ taakd/src/pages/Login.vue
@@ -1,33 +1,41 @@
 <template>
-  <div class="m-3 flex items-center justify-center">
-    <Card title="Login Taakd " class="w-full max-w-md mt-4">
-      <Logo class="flex flex-row items-center justify-center h-16 w-50 "></Logo>
-      <form class="flex flex-col space-y-2 w-full" @submit.prevent="submit">
-        <Input
-          required
-          name="email"
-          type="text"
-          placeholder="johndoe@email.com"
-          label="User ID"
-        />
-        <Input
-          required
-          name="password"
-          type="password"
-          placeholder="••••••"
-          label="Password"
-        />
-        <Button :loading="session.login.loading" variant="solid"
-        :class="['bg-secondary hover:bg-secondary_hover px-4 py-2 text-white']" >Login</Button
-        >
-      </form>
-    </Card>
+  <div class=" bg-bg  leading-[1.6rem] text-dark_gray">
+    <div class="m-3 h-screen w-screen flex items-center justify-center">
+      <Card title="Login Taakd " class="w-full max-w-[40rem] mt-4">
+        <Logo class="flex flex-row items-center justify-center "></Logo>
+        <form class="flex flex-col justify-start  w-full" @submit.prevent="submit">
+          <Input
+            required
+            name="email"
+            type="text"
+            placeholder="johndoe@email.com"
+            label="User ID"
+            class="p-5 py-6"
+
+          />
+          <Input
+            required
+            name="password"
+            type="password"
+            placeholder="••••••"
+            label="Password"
+            class="p-5 py-6"
+          />
+          <div class="px-5">
+            <Button :loading="session.login.loading" variant="solid"
+            :class="['bg-secondary hover:bg-secondary_hover block w-full h-full py-5 text-white ']" >Login</Button
+            >
+          </div>
+        </form>
+      </Card>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { session } from '../data/session'
 import Logo from '../components/Icons/Logo.vue';
+import BaseLayout from '../layouts/baseLayout.vue';
 
 
 function submit(e) {
