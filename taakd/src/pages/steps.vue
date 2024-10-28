3bed3b1515f3bebbd8dfc11a9206a059b6763302
abdoasofi <asofi@bonya-ye.org>
2024-10-28 10:50:01 +0300

    Edit Stayle Button Previous Step

--- taakd/src/pages/steps.vue
+++ taakd/src/pages/steps.vue
@@ -59,7 +59,7 @@
       <!-- Navigation Buttons -->
       <div   v-if="!isLastStep"  class="flex gap-2">
         <button 
-          class="flex items-center justify-center px-4 py-2 text-white bg-green-500 rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300" 
+          class="flex items-center justify-center px-4 py-2 text-white bg-secondary rounded-[100px]  hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300" 
           @click="previousStep" 
           :disabled="currentStepIndex === 0" 
           v-if="currentStepIndex > 0"
@@ -93,10 +93,11 @@
           {{ 'Step ' + currentStepIndex }} <!-- اسم المرحلة التي سيتم الرجوع إليها -->
         </button>
         
+
+        <Button level="other" @clicked="reject"> Reject </Button>
         <Button level="other" @clicked="accept" >
           Accept
         </Button>
-        <Button level="other" @clicked="reject"> Reject </Button>
       </div>
     </div>
   </div>
@@ -227,6 +228,7 @@ const goToStep = async(index) => {
     loading.value = true;
     
     if(currentStepIndex.value===1){
+      store.validateStep1
       await store.saveStep1();
     }
     else if(currentStepIndex.value===2){
