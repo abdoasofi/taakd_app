df997eb4a58567387f8576bdc2aa3e7636adf6c5
abdoasofi <asofi@bonya-ye.org>
2024-11-08 01:17:02 +0300

    Add function createAllRequestsList

--- taakd/src/data/request.ts
+++ taakd/src/data/request.ts
@@ -1,4 +1,3 @@
-// دوال التعامل مع Frappe UI:
 // src/data/request.ts
 import { createListResource, createDocumentResource, ResourceList, ResourceDocument } from 'frappe-ui';
 import { session } from './session';
@@ -12,6 +11,7 @@ interface CreateListResourceOptions {
   fields: string[];
   filters: { [key: string]: any };
   auto: boolean;
+  orderBy:string,
   pageLength: number;
 }
 
@@ -30,7 +30,30 @@ export function createRequestList(fields: string[]): ResourceList<RequestData> {
     fields,
     filters: { user_id: session.user },
     auto: true,
+    orderBy:'',
     pageLength: 1,
+  };
+
+  return createListResource<RequestData>(options);
+}
+
+/**
+ * إنشاء مورد قائمة لجميع طلبات Verification Instructions Request.
+ * @param fields - قائمة أسماء الحقول المراد جلبها.
+ * @returns مورد قائمة من نوع ResourceList<RequestData>
+ */
+export function createAllRequestsList(fields: string[]): ResourceList<RequestData> {
+  if (!session.user) {
+    throw new Error("User session is not available.");
+  }
+
+  const options: CreateListResourceOptions = {
+    doctype: 'Verification Instructions Request',
+    fields,
+    filters: { user_id: session.user },
+    auto: true,
+    orderBy: 'creation desc',
+    pageLength: 0,  // نحدد pageLength إلى 0 لجلب جميع السجلات
   };
 
   return createListResource<RequestData>(options);
@@ -46,11 +69,7 @@ export async function updateFieldsInRequestList(
   requestList: ResourceList<RequestData>,
   updatedFields: UpdateFields
 ): Promise<void> {
-  // if (!requestList || !requestList.data || requestList.data.length === 0) {
-  //   throw new Error("Request list data is not available");
-  // }
-
-  const documentName: string = '';
+  const documentName: string = ''; // يجب تحديد اسم الوثيقة هنا
 
   const document: ResourceDocument<RequestData> | undefined = requestList.documents.find(doc => doc.name === documentName);
 
@@ -77,6 +96,5 @@ export function getDocumentResource(name: string = ''): ResourceDocument<Request
   return createDocumentResource<RequestData>({
     doctype: "Verification Instructions Request",
     name: name,
-    // auto: true // إذا كنت ترغب في التفعيل التلقائي، قم بإزالة التعليق
   });
 }
